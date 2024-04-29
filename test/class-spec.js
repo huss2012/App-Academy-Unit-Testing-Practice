c7e22e8e9cb202521032ce2271ca2b420dc3b22c
huss2012 <huss24940@gmail.com>
2024-04-29 21:16:30 +0300

    class-specs have been written.

--- test/class-spec.js
+++ test/class-spec.js
@@ -6,29 +6,37 @@ const { Word } = require("../class");
 describe("Word", function () {
   describe("Word constructor function", function () {
     it('should have a "word" property', function () {
-      expect.fail("replace with your code");
+      let word = new Word("word");
+      expect(word.word).to.exist;
     });
-  
+
     it('should set the "word" property when a new word is created', function () {
-      expect.fail("replace with your code");
+      let word = new Word("word");
+      expect(word.word).to.equal("word");
     });
   });
 
   describe("removeVowels function", function () {
     it("should return a the word with all vowels removed", function () {
-      expect.fail("replace with your code");
+      let word = new Word("Apple");
+      expect(word.removeVowels()).to.equal("ppl");
     });
   });
 
   describe("removeConsonants function", function () {
     it("should return the word with the consonants removed", function () {
-      expect.fail("replace with your code");
+      let word = new Word("Apple");
+      expect(word.removeConsonants()).to.equal("Ae");
     });
   });
-  
+
   describe("pigLatin function", function () {
     it("should return the word converted to pig latin", function () {
-      expect.fail("replace with your code");
+      let word = new Word("apple");//bgins with vowles.
+      let word2 = new Word("people")//vowles in between.
+
+      expect(word.pigLatin()).to.equal("appleyay");
+      expect(word2.pigLatin()).to.equal("eoplepay")
     });
   });
 });
